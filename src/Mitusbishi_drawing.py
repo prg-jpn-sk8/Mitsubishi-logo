c1dbfe3bd7b2e988174354d89eb31645f855b23a
prg-jpn-sk8 <121560023+prg-jpn-sk8@users.noreply.github.com>
2022-12-30 19:47:41 +0300

    Add files via upload

---
+++ src/Mitusbishi_drawing.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+goto(0, 0)
+
+hideturtle()
+
+pencolor("red")
+
+speed(0.75)
+
+begin_fill()
+for x in range(3):
+    forward(100)
+    right(60)
+    forward(100)
+    right(120)
+    forward(100)
+    right(60)
+    forward(100)
+    fillcolor("red")
+
+end_fill()
+
+penup()
+goto(-265, -220)
+pendown()
+pencolor("black")
+write("MITSUBISHI", font=("Verdana", 70))
+done()
